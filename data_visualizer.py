8c3656b479985ec8e760cc18a1565881c5974c68
Florian Bauernfeind <e11775814@student.tuwien.ac.at>
2021-11-08 17:36:25 +0100

    add minimalistic styling to image showing for abnormal plots

--- data_visualizer.py
+++ data_visualizer.py
@@ -5,6 +5,9 @@ def show_images(image_paths, num, path_prefix = "data/images/iu_xray/"):
     nested_list = [elem.split(";") for elem in image_paths]
     flattened_list = [item for sublist in nested_list for item in sublist]
     for i in range(num):
-        img = mpimg.imread(path_prefix + flattened_list[i])
-        plt.imshow(img)
+        image_file_path = flattened_list[i]
+        img = mpimg.imread(path_prefix + image_file_path)
+        cax = plt.imshow(img)
+        plt.title("Abnormal image", fontsize=25)
+        plt.xlabel("File: {}".format(image_file_path))
         plt.show()
