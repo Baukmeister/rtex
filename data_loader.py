4405b61ce34ef9e6b467ebef27e3b6016a1aad1e
Florian Bauernfeind <e11775814@student.tuwien.ac.at>
2021-11-08 15:55:15 +0100

    recreate RTEX@R

--- data_loader.py
+++ data_loader.py
@@ -1,17 +1,26 @@
-import dload
+import gdown
 import os
-
+from pathlib import Path
+import shutil
 
 def load_data():
     """
     Download the required data used for RTEX classification (if they are not already present)
     """
+    Path("data").mkdir(parents=True, exist_ok=True)
     if not os.path.isdir("data/tags"):
         print("Downloading image tags ...")
-        dload.save_unzip("https://drive.google.com/uc?id=1nubphDVrKpB3Ss9uNaxHLUf2DWpqWRzq", "data/tags")
+        gdown.download("https://drive.google.com/uc?id=1nubphDVrKpB3Ss9uNaxHLUf2DWpqWRzq", "data/tags.zip")
+        shutil.unpack_archive("data/tags.zip", "data/tags")
         print("Finished downloading image tags!")
-        os.remove("uc")
     if not os.path.isdir("data/images"):
         print("Downloading images (this may take a while) ...")
-        dload.save_unzip("https://openi.nlm.nih.gov/imgs/collections/NLMCXR_png.tgz", "data/images")
+        gdown.download("https://openi.nlm.nih.gov/imgs/collections/NLMCXR_png.tgz", "data/images.zip")
+        shutil.unpack_archive("data/images.zip", "data/images")
         print("Finished downloading images!")
+    if not os.path.isdir("data/models"):
+        print("Downloading models ...")
+        gdown.download("https://drive.google.com/uc?id=1D8oHHSib1k8QHnDD_LLzf6lLbyjrtfOm", "data/models.zip")
+        shutil.unpack_archive("data/models.zip", "data/models")
+        print("Finished downloading models!")
+
--- explainability.py
+++ explainability.py
@@ -1,5 +1,5 @@
-
 import pandas as pd
+from keras.models import load_model
 
 import data_handler
 import data_loader
@@ -15,7 +15,19 @@ train_cases_tags = dict(zip(train.reports, train.mti_tags))
 train_cases_captions = dict(zip(train.reports, train.captions))
 
 test = pd.read_csv("data/tags/iu_xray_all_test.tsv", sep="\t")
-test_cases_images = dict(zip(test.reports,test.images))
+test_cases_images = dict(zip(test.reports, test.images))
 test_case_ids = list(test_cases_images.keys())
 
 x_test = data_handler.encode_images(test_cases_images, "data/images/iu_xray")
+
+bi_cxn = load_model("data/models/iu_xray_bi_cxn.hdf5")
+
+test_abn_probs = bi_cxn.predict(x_test, batch_size=16, verbose=1).flatten()
+
+cases_probs = dict(zip(test_case_ids, test_abn_probs))
+# Sort all exams (a.k.a. cases)
+sorted_cases_probs = {k: v for k, v in sorted(cases_probs.items(), key=lambda item: item[1], reverse=True)}
+sorted_cases = list(sorted_cases_probs.keys())
+# Get the top 100 abnormal exams
+abnormal_cases_images = {case: test_cases_images[case] for case in sorted_cases[:100]}
+print(abnormal_cases_images)
